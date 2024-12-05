da94b748f6ba4d3c35e0a7e9ba00510da3af1bcb
DhruvKumar2005 <156638526+DhruvKumar2005@users.noreply.github.com>
2024-12-05 16:47:10 +0530

    Add files via upload

---
+++ prg1_arr.java
@@ -0,0 +1,10 @@
+public class prg1_arr{
+    public static void main(String[] args){
+        int arr[]= {10,20,30,40,50};
+        for(int i=0;i< arr.length;i++){
+            System.out.println("Element at index" +i+"is : "+arr[i]);
+        }
+
+
+    }
+}
